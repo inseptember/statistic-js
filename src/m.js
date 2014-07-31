a0f992e2f1ba0a64d71400036aea5ee88d043110
inseptember <inseptember@yeah.net>
2014-07-31 17:58:07 +0800

    add cdf , beta function

--- src/m.js
+++ src/m.js
@@ -103,9 +103,7 @@ var Stt = {};
 	};
 	Stt.util.extend(A, Stt.base);
 	
-	var M = Stt.Math = function(){
-		this.name = 'Math';
-	}
+	var M = Stt.Math = {};
 	
 	var I = Stt.Iterator = function(arr){
 		var me = this;
@@ -125,6 +123,20 @@ var Stt = {};
 		me.init.apply(me, arguments);
 	};
 	Stt.util.extend(S, Stt.base);
+	
+	var N = Stt.Number = function(){
+		var me = this;
+		Stt.base.apply(me, [{}]);
+		me.init.apply(me, arguments);
+	};
+	Stt.util.extend(N, Stt.base);
+	
+	var CF = M.ContinuedFraction = function(op){
+		var me = this;
+		Stt.base.apply(me, [op]);
+		me.init.apply(me, arguments);
+	};
+	Stt.util.extend(CF, Stt.base);
 })();
 
 (function(){
@@ -140,6 +152,14 @@ var Stt = {};
 		me._set('index', i + 1);
 		return me._get("arr").get(i);
 	};
+})();
+
+(function(){
+	var N = Stt.Number;
+	
+	N.prototype.init = function(){
+		
+	}
 })();
 
 (function(){
@@ -253,12 +273,27 @@ var Stt = {};
 (function(){
 	var M = Stt.Math;
 	
-	M.prototype.average = function(){
+	var B = M.Beta = {};
+	
+	var CF = M.ContinuedFraction;
+	
+	var N = Stt.Number;
+	
+	N.is = {
+		Infinity : function(v){
+			return v == Infinity || v == -Infinity;
+		},
+		Nan : function(v){
+			return isNaN(v);
+		}
+	};
+	
+	M.average = function(){
 		var length = 0, all = 0;
-		var result = this.sum.apply(this, arguments);
+		var result = M.sum.apply(M, arguments);
 		return result.value * 1.00 / result.length
 	};
-	M.prototype.sum = function(){
+	M.sum = function(){
 		var length = 0, all = 0;
 		for(var i=arguments.length - 1; i>=0; i--){
 			var arg = arguments[i]
@@ -272,25 +307,163 @@ var Stt = {};
 			length : length
 		};
 	};
-	M.prototype.variance = function(s, average){
-		var avr = average || this.average(s), p1 = 0, p0 = s.size();
+	M.variance = function(s, average){
+		var avr = average || M.average(s), p1 = 0, p0 = s.size()-1;
 		s.each(function(val){
 			p1 += Math.pow(val - avr, 2);
 		});
 		return p1/p0;
 	};
-	M.prototype.ttest = function(sp0, sp1){
-		var avr0 = sp0.avr || this.average(sp0.data), avr1 = sp1.avr || this.average(sp1.data), 
-			v0 = sp0.variance || this.variance(sp0.data, avr0),v1 = sp1.variance || this.variance(sp1.data, avr1);
+	M.gammar = function(n){
+		var _n = n-1;
+		return Math.sqrt(2 * Math.PI * _n) * Math.pow(_n/Math.E, _n);
+	};
+	M.gammarln = function(n){
+		var cst = [76.18009172947146,-86.50532032941677,24.01409824083091,-1.231739572450155,0.1208650973866179e-2,-0.5395239384953e-5];
+		var x, y, tmp, ser = 1.000000000190015;
+		y = x = n;
+		tmp = x + 5.5;
+		tmp -= (x+0.5)*Math.log(tmp);
+		for(var j=0; j<6; j++){
+			ser += cst[j]/++y;
+		}
+		return -tmp + Math.log(2.5066282746310005*ser/x);
+		
+	};
+	M.lnBeta = function(a, b){
+		return Math.log(Math.exp(M.gammarln(a)) * Math.exp(M.gammarln(b))/ Math.exp(M.gammarln(a + b)));
+	};
+	M.degreesFreedom = function(sp0, sp1){
+		var n0 = sp0.size, n1 = sp1.size, v0 = sp0.variance, v1 = sp1.variance,
+		s0 = v0/n0, s1 = v1/n1;
+		return Math.pow(s0 + s1, 2)/(s0*s0/(n0-1) + s1*s1/(n1-1))
+	};
+	M.tscore = function(sp0, sp1){
+		var avr0 = sp0.avr || M.average(sp0.data), avr1 = sp1.avr || M.average(sp1.data), 
+			v0 = sp0.variance || M.variance(sp0.data, avr0),v1 = sp1.variance || M.variance(sp1.data, avr1);
 		var p0 = avr0 - avr1, p1 = Math.sqrt(v0/sp0.size + v1/sp1.size);
 		return p0/p1;
 	};
+	M.ttest = function(sp0, sp1){
+		var t = M.tscore(sp0, sp1), df = M.degreesFreedom(sp0, sp1);
+		//return Math.pow(1 + t*t/df, -(df + 1)/2) * M.gammar((df + 1)/2)/(M.gammar(df/2)*Math.sqrt(df * Math.PI));
+		return 2.0 * M.cdf(-t, df);
+	};
+	M.cdf = function(x, df){
+		var ret = NaN;
+		if(x == 0){
+			ret = 0.5
+		}else{
+			var t = B.iBeta(df/(df + x*x), 0.5 * df, 0.5);
+			if(x < 0){
+				ret = 0.5 * t;
+			}else{
+				ret = 1.0 - 0.5 * t;
+			}
+		}
+		
+		return ret;
+	};
+	M.equals = function(x, y, maxUlps){
+		return Math.abs(x.toExponential(4) - y.toExponential(4)) <= maxUlps.toExponential(4)
+			|| Math.abs(x - y) <= maxUlps;
+	};
+	
+	B.iBeta = function(x, a, b, ep, max){
+		var r, epsilon = ep||1.0E-14, maxIterations = max||2147483647;
+		if(x<0 || x>1 || a<=0 || b<=0){
+			return NaN;
+		}else if(x>(a + 1)/(2+b+a) && (1-x)<=(b+1)/(2 + b+ a)){
+			r = 1 - B.iBeta(1-x, b, a, epsilon, maxIterations);
+		}else{
+			var cf = new CF({
+				getB : function(n, xx){
+					var rr, m;
+					if(n % 2 == 0){
+						m = n/2.0;
+						rr = (m *(b - m) * xx )/((a + 2 * m - 1)*(a + 2 * m));
+					}else{
+						m = (n - 1.0)/ 2.0;
+						rr = -((a + m) * (a + b + m) * xx)/((a + 2 * m) * (a + 2*m + 1))
+					}
+					return rr;
+				},
+				getA : function(n, xx){
+					return 1.0;
+				}
+			});
+			r = Math.exp(a * Math.log(x) + b * Math.log(1-x) - Math.log(a) - M.lnBeta(a, b)) * 1.0/cf.evaluate(x, epsilon, maxIterations);
+		}
+		
+		return r;
+	};
+	
+	CF.prototype.init = function(op){
+		
+	};
+	
+	CF.prototype.getA = function(){
+		return this._get('getA')||function(){};
+	};
+	
+	CF.prototype.getB = function(){
+		return this._get('getB')||function(){};
+	};
+	
+	CF.prototype.evaluate = function(x, epsilon, maxIterations){
+		var me = this, small = 1e-50;
+		var hPrev = me.getA().call(me, 0, x);
+		
+		if(M.equals(hPrev, 0.0, small)){
+			hPrev = small;
+		}
+		
+		var n =1, dPrev = 0.0, cPrev = hPrev, hN = hPrev;
+		while(n < maxIterations){
+			var a = me.getA().call(me, n, x);
+			var b = me.getB().call(me, n, x);
+			
+			var dN = a + b * dPrev;
+			if(M.equals(dN, 0.0, small)){
+				dN = small;
+			}
+			var cN = a + b/cPrev;
+			if(M.equals(cN, 0.0, small)){
+				cN = small;
+			}
+			
+			dN = 1/dN;
+			var deltaN = cN*dN;
+			hN = hPrev * deltaN;
+			
+			if(N.is.Infinity(hN)==true){
+				throw "Continued fraction convergents diverged to +/- infinity for value " + hN;
+			}
+			if(N.is.Nan(hN) == true){
+				throw "Continued fraction diverged to NaN for value " + hN;
+			}
+			
+			if(Math.abs(deltaN - 1.0) < epsilon){
+				break;
+			}
+			
+			dPrev = dN;
+			cPrev = cN;
+			hPrev = hN;
+			n ++;
+		}
+		if(n >= maxIterations){
+			throw "Continued fraction convergents failed to converge (in less than "+maxIterations+" iterations) for value " + x;
+		}
+		
+		return hN;
+	}
 })();
 
 (function(){
 	var A = Stt.Array;
 	var S = Stt.Sample;
-	var m = new Stt.Math()
+	var m = Stt.Math
 	S.prototype.init = function(){
 		var me = this;
 		var inner = new A();
